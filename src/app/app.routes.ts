13f7c225cb6c24a2e0775e430f6f41e7d9cf929f
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-02-26 23:14:50 -0600

    practice finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full' 
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about-us',
+        component: AboutUsComponent
+    }
+];
